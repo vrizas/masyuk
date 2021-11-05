fca5be37ec76ef2df27f0fb9b81a8fdab1d8afea
ekotyoo <ekeprasetyo@gmail.com>
2021-11-05 19:05:45 +0700

    Fixing daftar member admin-panel

--- app/Http/Livewire/Admin/DaftarMember.php
+++ app/Http/Livewire/Admin/DaftarMember.php
@@ -7,15 +7,22 @@ use Livewire\Component;
 
 class DaftarMember extends Component
 {
+    public $memberId = null;
     public function render()
     {
         $members = User::all();
         return view('livewire.admin.daftar-member', ['members' => $members]);
     }
 
-    public function deleteMember($id)
+    public function deleteMember()
     {
-        User::find($id)->delete();
+        User::find($this->memberId)->delete();
         session()->flash('message', 'Member berhasil dihapus.');
+        $this->memberId = null;
+    }
+
+    public function showDeleteModal($id)
+    {
+        $this->memberId = $id;
     }
 }
