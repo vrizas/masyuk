04b4228b21fd008add432f143cc1cc373d95e326
ekotyoo <ekeprasetyo@gmail.com>
2021-11-06 08:39:20 +0700

    Menambah field username pada user, hapus middleware auth pada home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Resep;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $resep = Resep::with('user')->get();
+        return view('home.home', ['reseps' => $resep]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ResepController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -15,15 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ResepController::class, 'getResepHome']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/admin', function () {
     return view('admin');
 });
 
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // http://127.0.0.1:8000/dennis
 Route::get('/dennis', function () {
