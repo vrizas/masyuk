a856cee037616e8e61938cf24318ddbaf8cdb4f6
vrizas <vrizasizza@gmail.com>
2021-11-07 18:53:20 +0700

    Membuat navigasi dropdown untuk fitur notifikasi.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,42 +18,85 @@
         <a href="/" class="flex items-center gap-2">
             <h1 class="text-xl font-bold">Masyuk</h1>
         </a>
-        <nav class="flex gap-4">
-            @if (!Auth::check())
+        @if (!Auth::check())
+            <nav class="flex items-center gap-4">
                 <a href="#login" class="btn btn-primary btn-rh py-2.5 w-20 capitalize">Login</a>
                 <a href="#signup" class="btn btn-outline btn-primary btn-rh py-2.5 w-20 capitalize">Signup</a>
             @else
-                <div class="dropdown dropdown-hover">
-                    <div tabindex="0">
-                        <div class="avatar">
-                            <div class="rounded-full w-9 h-9">
-                                <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                <nav class="flex items-center gap-8">
+                    <div class="dropdown dropdown-end">
+                        <button tabindex="0">
+                            <i class='bx bxs-bell text-2xl indicator'>
+                                <div class="indicator-item badge text-xs px-2 bg-primary-focus border-none rounded-full">
+                                    3
+                                </div>
+                            </i>
+                        </button>
+                        <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-96 text-sm">
+                            <li>
+                                <a>
+                                    <div class="avatar mr-2">
+                                        <div class="rounded-full w-7 h-7">
+                                            <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                                        </div>
+                                    </div>
+                                    <p>Eko Prasetyo mulai mengikuti Anda.</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <div class="avatar mr-2">
+                                        <div class="rounded-full w-7 h-7">
+                                            <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                                        </div>
+                                    </div>
+                                    <p>Vrizas Izza mulai mengikuti Anda.</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <div class="avatar mr-2">
+                                        <div class="rounded-full w-7 h-7">
+                                            <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                                        </div>
+                                    </div>
+                                    <p>Ivanna mulai mengikuti Anda.</p>
+                                </a>
+                            </li>
+                            <a href="" class="text-right mt-2 mb-4 mr-4">Lihat Selengkapnya</a>
+                        </ul>
+                    </div>
+                    <div class="dropdown dropdown-hover">
+                        <div tabindex="0">
+                            <div class="avatar">
+                                <div class="rounded-full w-9 h-9">
+                                    <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                                </div>
                             </div>
                         </div>
+                        <ul tabindex="0"
+                            class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52 right-0 text-sm">
+                            <li>
+                                <a href="/profile/{{ Auth::user()->username }}">Profile</a>
+                            </li>
+                            <li>
+                                <a>Item 2</a>
+                            </li>
+                            <li>
+                                <a>Item 3</a>
+                            </li>
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit"
+                                        class="btn btn-error btn-rh py-2.5 w-20 capitalize w-full mt-2">Logout</button>
+                                </form>
+                            </li>
+                        </ul>
+
                     </div>
-                    <ul tabindex="0"
-                        class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52 right-0 text-sm">
-                        <li>
-                            <a href="/profile/{{ Auth::user()->username }}">Profile</a>
-                        </li>
-                        <li>
-                            <a>Item 2</a>
-                        </li>
-                        <li>
-                            <a>Item 3</a>
-                        </li>
-                        <li>
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit"
-                                    class="btn btn-error btn-rh py-2.5 w-20 capitalize w-full mt-2">Logout</button>
-                            </form>
-                        </li>
-                    </ul>
 
-                </div>
-
-            @endif
+        @endif
         </nav>
     </header>
     @yield('main')
