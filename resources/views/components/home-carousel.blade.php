3ca84bb6eb4b987d39a2f14c4db8943e107cb84e
ekotyoo <ekeprasetyo@gmail.com>
2021-11-09 02:07:00 +0700

    fix image path

--- resources/views/components/home-carousel.blade.php
+++ resources/views/components/home-carousel.blade.php
@@ -2,7 +2,11 @@
     @foreach ($reseps as $key => $resep)
         <div id="slide{{ $key + 1 }}" class="relative w-full carousel-item">
             <div class="mx-20 flex gap-8 z-30">
-                <img src="{{ $resep->imageUrl }}" class="rounded-2xl w-7/12 h-80 object-cover">
+                @if ($resep->photos()->first() != null)
+                    <img src="{{ $resep->imageUrl }}" class="rounded-2xl w-7/12 h-80 object-cover">
+                @else
+                    <img src="http://blog.sayurbox.com/wp-content/uploads/2021/03/edisibelajarmasak-595x375.jpg" class="rounded-2xl w-7/12 h-80 object-cover">
+                @endif
                 <div class="text">
                     <p>Resep of the day</p>
                     <h2 class="font-bold text-4xl my-3">{{ $resep->title }}</h2>
--- resources/views/components/resep-card.blade.php
+++ resources/views/components/resep-card.blade.php
@@ -1,10 +1,16 @@
 <a class="h-auto card-masyuk" href="/reseps/{{ $resep->id }}">
-  <div class="relative">
-    <img src="{{ $resep->imageUrl }}" alt="Image 1" class="rounded-2xl">
-    <div class="from-black bg-gradient-to-t w-full rounded-2xl absolute top-0 left-0 image-filter opacity-50"></div>
-      <div class="text-white rounded-2xl absolute w-full top-0 left-0 z-30 p-4 flex flex-col justify-end">
-        <h3 class="font-bold text-xl">{{ $resep->title }}</h3>
-        <p class="font-medium">{{ $resep->user->name ?? 'No User Owned This Recipe' }}</p>
+    <div class="relative">
+        @if ($resep->photos()->first() != null)
+            <img src="{{ asset('/storage/photos/' . $resep->photos()->first()->filename) }}" alt="Image 1"
+                class="rounded-2xl">
+        @else
+            <img src="http://blog.sayurbox.com/wp-content/uploads/2021/03/edisibelajarmasak-595x375.jpg" alt="Image 1"
+                class="rounded-2xl">
+        @endif
+        <div class="from-black bg-gradient-to-t w-full rounded-2xl absolute top-0 left-0 image-filter opacity-50"></div>
+        <div class="text-white rounded-2xl absolute w-full top-0 left-0 z-30 p-4 flex flex-col justify-end">
+            <h3 class="font-bold text-xl">{{ $resep->title }}</h3>
+            <p class="font-medium">{{ $resep->user->name ?? 'No User Owned This Recipe' }}</p>
+        </div>
     </div>
-  </div> 
 </a>
