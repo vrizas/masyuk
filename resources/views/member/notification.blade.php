ff20e4ea44882952d248afb10e4429576092a908
vrizas <vrizasizza@gmail.com>
2021-11-13 11:19:44 +0700

    Memperbaiki halaman notifikasi berdasarkan timeline dan menghapus wire model getNotifications pada notification indicator.

--- resources/views/member/notification.blade.php
+++ resources/views/member/notification.blade.php
@@ -4,10 +4,11 @@
     @livewireStyles
 @endsection
 @section('main')
-    <main class="py-4">
+    <main class="py-2">
         <article>
-            <h2 class="font-bold text-lg mb-2">Notifikasi</h2>
+            <h2 class="font-bold text-lg mb-2"><i class="bi bi-bell-fill text-xl mr-1"></i> Notifikasi</h2>
             <section class="flex flex-col gap-1">
+                <p class="font-medium text-sm">Hari Ini</p>
                 <div class="px-3 py-2 shadow bg-base-100 rounded-lg text-sm flex gap-2 items-center relative">
                     <div class="badge badge-primary badge-xs self-start mt-1 mr-1"></div>
                     <a href="#" class="avatar mr-2">
@@ -17,7 +18,7 @@
                     </a>
                     <div class="flex flex-col gap-0.5">
                         <p><b>Vrizas Izza</b> menyukai resep Anda.</p>
-                        <p class="text-xs font-md text-gray-700">10 menit yang lalu</p>
+                        <p class="text-xs font-medium text-gray-700">10 menit yang lalu</p>
                     </div>
                     <button></button>
                     <div data-tip="Tandai sudah terbaca"
@@ -34,11 +35,11 @@
                     </a>
                     <div class="flex flex-col gap-0.5">
                         <p><b>Bambang</b> mengomentari resep Anda.</p>
-                        <p class="text-xs font-md text-gray-700">Udah coba bikin, enak bangett</p>
-                        <p class="text-xs font-md text-gray-700">17 November 2021</p>
+                        <p class="text-xs text-gray-700">Udah coba bikin, enak bangett</p>
+                        <p class="text-xs font-medium text-gray-700">17 November 2021</p>
                     </div>
                     <div class="absolute top-1/2 right-3 transform -translate-y-1/2">
-                        <button></i><i class="bi bi-envelope-open text-base"></i></button>
+                        <div></i><i class="bi bi-envelope-open text-base"></i></div>
                     </div>
                 </div>
                 <div class="px-3 py-2 shadow bg-base-100 rounded-lg text-sm flex gap-2 items-center relative">
@@ -50,11 +51,63 @@
                     </a>
                     <div class="flex flex-col gap-0.5">
                         <p><b>Basri</b> mengomentari resep Anda.</p>
-                        <p class="text-xs font-md text-gray-700">enak bangett</p>
-                        <p class="text-xs font-md text-gray-700">17 November 2021</p>
+                        <p class="text-xs text-gray-700">enak bangett</p>
+                        <p class="text-xs font-medium text-gray-700">17 November 2021</p>
                     </div>
                     <div class="absolute top-1/2 right-3 transform -translate-y-1/2">
-                        <button></i><i class="bi bi-envelope-open text-base"></i></button>
+                        <div></i><i class="bi bi-envelope-open text-base"></i></div>
+                    </div>
+                </div>
+            </section>
+            <section class="flex flex-col gap-1 mt-4">
+                <p class="font-medium text-sm">Kemarin</p>
+                <div class="px-3 py-2 shadow bg-base-100 rounded-lg text-sm flex gap-2 items-center relative">
+                    <div class="badge badge-primary badge-xs self-start mt-1 mr-1"></div>
+                    <a href="#" class="avatar mr-2">
+                        <div class="rounded-full w-8 h-8">
+                            <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                        </div>
+                    </a>
+                    <div class="flex flex-col gap-0.5">
+                        <p><b>Vrizas Izza</b> menyukai resep Anda.</p>
+                        <p class="text-xs font-medium text-gray-700">10 menit yang lalu</p>
+                    </div>
+                    <button></button>
+                    <div data-tip="Tandai sudah terbaca"
+                        class="tooltip tooltip-left absolute top-1/2 right-3 transform -translate-y-1/2">
+                        <button></i><i class="bi bi-envelope text-base"></i></button>
+                    </div>
+                </div>
+                <div class="px-3 py-2 shadow bg-base-100 rounded-lg text-sm flex gap-2 items-center relative">
+                    <div class="badge bg-gray-300 border-gray-300 badge-xs self-start mt-1 mr-1"></div>
+                    <a href="#" class="avatar mr-2">
+                        <div class="rounded-full w-8 h-8">
+                            <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                        </div>
+                    </a>
+                    <div class="flex flex-col gap-0.5">
+                        <p><b>Bambang</b> mengomentari resep Anda.</p>
+                        <p class="text-xs text-gray-700">Udah coba bikin, enak bangett</p>
+                        <p class="text-xs font-medium text-gray-700">17 November 2021</p>
+                    </div>
+                    <div class="absolute top-1/2 right-3 transform -translate-y-1/2">
+                        <div></i><i class="bi bi-envelope-open text-base"></i></div>
+                    </div>
+                </div>
+                <div class="px-3 py-2 shadow bg-base-100 rounded-lg text-sm flex gap-2 items-center relative">
+                    <div class="badge bg-gray-300 border-gray-300 badge-xs self-start mt-1 mr-1"></div>
+                    <a href="#" class="avatar mr-2">
+                        <div class="rounded-full w-8 h-8">
+                            <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                        </div>
+                    </a>
+                    <div class="flex flex-col gap-0.5">
+                        <p><b>Basri</b> mengomentari resep Anda.</p>
+                        <p class="text-xs text-gray-700">enak bangett</p>
+                        <p class="text-xs font-medium text-gray-700">17 November 2021</p>
+                    </div>
+                    <div class="absolute top-1/2 right-3 transform -translate-y-1/2">
+                        <div></i><i class="bi bi-envelope-open text-base"></i></div>
                     </div>
                 </div>
             </section>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -30,7 +30,7 @@ Route::get('/reseps/{id}', [ResepController::class, 'show']);
 
 Route::get('profile/{user:username}', [ProfileController::class, 'show'])->name('profile');
 
-Route::get('/notification', function () {
+Route::get('/notifications', function () {
     return view('member.notification');
 })->middleware('auth');
 
